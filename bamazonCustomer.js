f3171ba6608a2c4876812f369f4bf266b680aba3
Thea Wilson <theawilson@Thea-Wilsons-MacBook-Pro-2.local>
2017-01-13 00:02:35 -0600

    Added promise.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,40 +2,37 @@ var mysql = require('mysql');
 var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
-	user: 'root',
-	password: 'password',
-	database: 'bamazon_db'
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'password',
+  database: 'bamazon_db'
 });
 
-connection.connect(function(err){
-	if (err) throw err;
-	console.log('Connected as id: ' + connection.threadId);
-	start();
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('Connected as id: ' + connection.threadId);
+  start();
 });
 
-var start = function(){
-	console.log('Welcome to Bamazon!');
-	console.log('Here is our current inventory.');
-	new Promise(function () {
-		return searchAll();
-	}).then(function() {
-		console.log('hello');
-	}).catch(function(err) {
-		console.log(err);
-	});
-}
-
-var searchAll = function() {
-	connection.query('SELECT * FROM bamazon_db.products', function(err, res){
-		if (err) throw err;
-		for (var i = 0; i < res.length; i++) {
-			console.log('ID#: ' + res[i].item_id + ' "' + res[i].product_name + '" Price: ' + res[i].price);
-		}
-	});
+var start = function() {
+  console.log('Welcome to Bamazon!');
+  console.log('Here is our current inventory.');
+  new Promise(function(resolve, reject) {
+    connection.query('SELECT * FROM products', function(err, res) {
+      if (err) throw err;
+      resolve(res);
+      for (var i = 0; i < res.length; i++) {
+        console.log('ID#: ' + res[i].item_id + ' "' + res[i].product_name + '" Price: ' + res[i].price);
+      }
+    });
+  }).then(function() {
+  	console.log('after promise');
+  }).catch(function(reject) {
+    console.log(reject);
+  });
 }
 
 var makePurchase = function() {
-	
+
 }
